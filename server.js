5d852a0556bc4f48f3c82d88b441b2a824f837de
Michael <M.Flower@gmx.de>
2017-09-11 18:41:23 +0200

    Place server into angular project structure.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db');
 
 const app = express();
 
@@ -18,7 +17,7 @@ MongoClient.connect(db.url, (err, database) => {
     if (err) {
         return console.log(err);
     }
-    require('./app/routes')(app, database);
+    require('./server/routes')(app, database);
 
     app.listen(port, () => {
         console.log('We are live on ' + port);
