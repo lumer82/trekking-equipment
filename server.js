695cb7bd295b2ecb027a562f7bbbddca22102947
Michael <M.Flower@gmx.de>
2017-09-11 18:49:49 +0200

    Modify server to use env variable for mongodb connection.

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 8000;
-
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-MongoClient.connect(db.url, (err, database) => {
-    if (err) {
-        return console.log(err);
-    }
-    require('./server/routes')(app, database);
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
-    app.listen(port, () => {
-        console.log('We are live on ' + port);
-    });
+MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  if (err) {
+    return console.log(err);
+  }
+  require('./server/routes')(app, database);
+
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log('We are live on ' + port);
+  });
 })
