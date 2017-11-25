8e37a924f81b8670064ad25b9c97ed73b26b4581
Michael <M.Flower@gmx.de>
2017-11-25 12:01:39 +0100

    Enable Entry Update

--- src/app/equipment-set/equipment-entry/equipment-entry.component.ts
+++ src/app/equipment-set/equipment-entry/equipment-entry.component.ts
@@ -1,9 +1,12 @@
+import { UpdateEquipmentEntryAction } from './../store/actions/equipment-entry.actions';
+import { debounceTime } from 'rxjs/operators/debounceTime';
 import { EquipmentCollection } from './../../shared/models/equipment-collection.model';
 import { Store } from '@ngrx/store';
 import { EquipmentEntry } from './../../shared/models/equipment-entry.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { EquipmentSetState } from '../store/equipment-set.reducer';
 import { DeleteEquipmentEntryAction } from '../store/actions/equipment-entry.actions';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'equip-equipment-entry',
@@ -11,6 +14,8 @@ import { DeleteEquipmentEntryAction } from '../store/actions/equipment-entry.act
   styleUrls: ['./equipment-entry.component.scss']
 })
 export class EquipmentEntryComponent implements OnInit {
+
+  nameForm: FormControl;
 
   @Input()
   entry: EquipmentEntry;
@@ -21,6 +26,12 @@ export class EquipmentEntryComponent implements OnInit {
   constructor(private store: Store<{ equipmentSet: EquipmentSetState }>) { }
 
   ngOnInit() {
+    this.nameForm = new FormControl();
+    this.nameForm.valueChanges
+      .pipe(
+        debounceTime(800)
+      )
+      .subscribe(name => this.store.dispatch(new UpdateEquipmentEntryAction({...this.entry, name})));
   }
 
   delete(): void {
